8dd8105bd56bfbed9642bcd78de37a38e6a3f33d
Antonio Ojea <antonio.ojea.garcia@gmail.com>
2018-09-07 00:44:11 +0200

    WIP Add integration tests

---
+++ tests/integration/run_tests.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -ev
+
+# Precheck
+
+psql2mysql.py --source postgresql://neutron:secret@localhost/neutron precheck
+
+# Migrate 
+
+psql2mysql.py \
+    --source postgresql://neutron:secret@localhost/neutron \
+    --target mysql+pymysql://neutron:evenmoresecret@localhost/neutron?charset=utf8 \
+    migrate
+
+# Verify migration was succesful
+
+mysqldump -u neutron -pevenmoresecret neutron > neutron_migrated.sql
+
+if ! cmp neutron.sql neutron_migrated.sql
+then
+  echo "Database Migration Failed"
+  exit 1
+fi
+
